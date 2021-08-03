f73ad5ffe6519b8af7c6879a81d93f0dd6ceb358
ron33333 <86957511+ron33333@users.noreply.github.com>
2021-08-03 14:13:36 +0300

    Update Initials.js

--- src/Initials/Initials.js
+++ src/Initials/Initials.js
@@ -1,5 +1,5 @@
 export default function Initials (props) {
     const splitted = props.name.split(' ');
-    const initials = splitted[0][0]+'.'+splitted[1][0];
+    const initials = splitted[0][0] +'.'+ splitted[1][0];
     return <div>{initials}</div>
 }
