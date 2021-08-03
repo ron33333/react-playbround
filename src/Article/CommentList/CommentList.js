22f53da33a75436ee4de544dca0d1db6b8e95829
ron33333 <86957511+ron33333@users.noreply.github.com>
2021-08-03 14:10:51 +0300

    Update CommentList.js

--- src/Article/CommentList/CommentList.js
+++ src/Article/CommentList/CommentList.js
@@ -1,9 +1,9 @@
 import Comment from './Comment/Comment.js'
 export default function CommentList(){
    return (<div>
-             <Comment weather='Hot' author={'Yarin'}/>
+             <Comment weather='Hot' author={'Rpn'}/>
              <Comment weather='Cold' author={'Moshe'}/>
-             <Comment weather='Tempered' author={'Gil'}/>
+             <Comment weather='Tempered' author={'Gadi'}/>
           </div>
    )
 }
