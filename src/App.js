a0d34241fab21d23bc5a0781f161ccf5a3c7e33b
ron33333 <86957511+ron33333@users.noreply.github.com>
2021-08-03 13:57:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+// eslint-disable-next-line
+import Initials from './Initials/Initials';
+// eslint-disable-next-line
+import DayOfWeek from "./DayOfWeek/DayOfWeek"
+// eslint-disable-next-line
+import Article from "./Article/Article"
+// eslint-disable-next-line
+import Menu from "./Menu/Menu"
+import MenuItem from "./Menu/MenuItem/MenuItem"
+// eslint-disable-next-line
+import Counter from "./Counter/Counter"
+// eslint-disable-next-line
+import StudentPicker from "./StudentPicker/StudentPicker"
+// eslint-disable-next-line
+import RandomColorSquare from "./RandomColorSquare/RandomColorSquare";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    // eslint-disable-next-line
+    function clickHandler() {
+        console.log('you clicked');
+        }
+ 
+    return (
+        <div>
+            {/*<button onClick={clickHandler}>Click me</button>*/}
+            {/*<Counter />*/}
+            {/*<StudentPicker/>*/}
+            <div>
+                <Menu>
+                    <h1>Menu</h1>
+                    <MenuItem>
+                        <a
+                            href={'/Content'}>
+                            Content
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a
+                            href={'/Gallery'}>
+                            Gallery
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a href={'/Contact'}>
+                            Contact
+                        </a>
+                    </MenuItem>
+                </Menu>
+            </div>
+            <div>
+            <RandomColorSquare>
+
+            </RandomColorSquare>
+            </div>
+        {/*<Initials name='Ron Mikles'/>*/}
+        {/*<DayOfWeek day={1}/>*/}
+        {/*  <Article title='Bla Bla' Content='Bla Bla Bla?'/>*/}
+        </div>
+    );
+    }
 
 export default App;
